938fcbd3bc533c513d893b57f5766d5fd2102251
sokolov1990 <82565779+sokolov1990@users.noreply.github.com>
2021-09-10 12:58:36 +0300

    Add files via upload

---
+++ js/level3.js
@@ -0,0 +1,101 @@
+let a = 0;
+
+
+
+
+
+
+function change1() {
+    if (document.querySelector('.btn1').style.backgroundColor == "rgb(207, 128, 31)")
+        document.querySelector('.btn1').style.backgroundColor = "yellow";
+
+    else if (document.querySelector('.btn1').style.backgroundColor == "yellow")
+        document.querySelector('.btn1').style.backgroundColor = "rgb(207, 128, 31)";
+}
+
+function change2() {
+    if (document.querySelector('.btn2').style.backgroundColor == "rgb(207, 128, 31)")
+        document.querySelector('.btn2').style.backgroundColor = "yellow";
+
+    else if (document.querySelector('.btn2').style.backgroundColor == "yellow")
+        document.querySelector('.btn2').style.backgroundColor = "rgb(207, 128, 31)";
+
+}
+
+function change3() {
+    if (document.querySelector('.btn3').style.backgroundColor == "rgb(207, 128, 31)")
+        document.querySelector('.btn3').style.backgroundColor = "yellow";
+
+    else if (document.querySelector('.btn3').style.backgroundColor == "yellow")
+        document.querySelector('.btn3').style.backgroundColor = "rgb(207, 128, 31)";
+}
+
+function change4() {
+    if (document.querySelector('.btn4').style.backgroundColor == "rgb(207, 128, 31)")
+        document.querySelector('.btn4').style.backgroundColor = "yellow";
+
+    else if (document.querySelector('.btn4').style.backgroundColor == "yellow")
+        document.querySelector('.btn4').style.backgroundColor = "rgb(207, 128, 31)";
+}
+
+function change5() {
+    if (document.querySelector('.btn5').style.backgroundColor == "rgb(207, 128, 31)")
+        document.querySelector('.btn5').style.backgroundColor = "yellow";
+
+    else if (document.querySelector('.btn5').style.backgroundColor == "yellow")
+        document.querySelector('.btn5').style.backgroundColor = "rgb(207, 128, 31)";
+}
+
+function change6() {
+    if (document.querySelector('.btn6').style.backgroundColor == "rgb(207, 128, 31)")
+        document.querySelector('.btn6').style.backgroundColor = "yellow";
+
+    else if (document.querySelector('.btn6').style.backgroundColor == "yellow")
+        document.querySelector('.btn6').style.backgroundColor = "rgb(207, 128, 31)";
+}
+
+function change7() {
+    if (document.querySelector('.btn7').style.backgroundColor == "rgb(207, 128, 31)")
+        document.querySelector('.btn7').style.backgroundColor = "yellow";
+
+    else if (document.querySelector('.btn7').style.backgroundColor == "yellow")
+        document.querySelector('.btn7').style.backgroundColor = "rgb(207, 128, 31)";
+}
+
+function change8() {
+    if (document.querySelector('.btn8').style.backgroundColor == "rgb(207, 128, 31)")
+        document.querySelector('.btn8').style.backgroundColor = "yellow";
+
+    else if (document.querySelector('.btn8').style.backgroundColor == "yellow")
+        document.querySelector('.btn8').style.backgroundColor = "rgb(207, 128, 31)";
+}
+
+function change9() {
+    if (document.querySelector('.btn9').style.backgroundColor == "rgb(207, 128, 31)")
+        document.querySelector('.btn9').style.backgroundColor = "yellow";
+
+    else if (document.querySelector('.btn9').style.backgroundColor == "yellow")
+        document.querySelector('.btn9').style.backgroundColor = "rgb(207, 128, 31)";
+}
+
+
+function result() {
+
+    if (document.querySelector('.btn3').style.backgroundColor == "yellow" &&
+        document.querySelector('.btn5').style.backgroundColor == "yellow" &&
+        document.querySelector('.btn6').style.backgroundColor == "yellow" &&
+        document.querySelector('.btn7').style.backgroundColor == "yellow") {
+        document.querySelector('.yes').style.display = "block";
+        document.querySelector('.container').style.display = "none";
+    } else {
+        document.querySelector('.no').style.display = "block";
+        document.querySelector('.container').style.display = "none";
+    }
+}
+
+function go() {
+    document.querySelector('.yes').style.display = "none";
+    document.querySelector('.container').style.display = "none";
+
+
+}
